db23e6f7dd416a6010ff4ed613ddc9cae083315c
Anderson Andrade <anderson_andrade_@outlook.com>
2025-09-25 00:09:52 -0300

    chore(second exercise list): implement comentaries to implement necessary code to second list

---
+++ second-exercise-list.ts
@@ -0,0 +1,19 @@
+// transforme uma funcao que soma dois numeros em uma funcao de duas etapas (curried)
+
+// implemente as funcoes pipe e compose para uma pipeline de funcoes
+
+// flatten com reduce -> implemente uma funcao que achata arrays de multiplos niveis em um unico array
+
+//  groupBy -> Crie uma funcao que agrupe usuarios por idade ou por inicial do nome
+
+// map personalizado -> reimplemente o comportamento do map usando apenas reduce
+
+// filter personalizado -> reimplemente o comportamento do filter usando apenas reduce
+
+// contador de palavars -> dada uma string, conte quantas vezes cada palavra aparece
+
+// imutabilidade profunda -> Dado um objeto de usuários com endereço, crie uma função que altere apenas a cidade de um usuário sem modificar o objeto original.
+
+// funcao uniq -> Implemente uma função que remove valores duplicados de um array sem usar Set.
+
+// async pipe -> Implemente um asyncPipe que permita encadear funções assíncronas.
