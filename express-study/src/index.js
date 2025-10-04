83f4c17e682670d278a684c1feb342ac9843e860
Anderson Andrade <anderson_andrade_@outlook.com>
2025-10-04 14:19:27 -0300

    feat(express) add basic configuration to express
    the class build and start express

---
+++ express-study/src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+class App {
+    expressApp;
+
+    constructor(){
+        this.buildApp();
+    }
+
+    buildApp(){
+        this.expressApp = express();
+    }
+    
+    startServer(){
+        const DEFAULT_PORT = 3000;
+
+        this.expressApp.listen(DEFAULT_PORT, () => console.log(`Server running on http://localhost:${DEFAULT_PORT}`));
+    }
+}
+
+export const application = new App().startServer();
