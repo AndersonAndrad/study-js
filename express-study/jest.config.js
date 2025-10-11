41bdc38a9e1c4a428a092df1eb56d491cd7c75ab
Anderson Andrade <anderson_andrade_@outlook.com>
2025-10-11 11:51:07 -0300

    chore(jest): add script to run coverage test

--- express-study/jest.config.js
+++ express-study/jest.config.js
@@ -1,5 +1,19 @@
 export default {
-  transform: { '^.+\\.jsx?$': 'babel-jest' },
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.spec.js'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.config.js',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
